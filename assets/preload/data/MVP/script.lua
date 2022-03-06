a798a9f40dd0a57f5a9e480461f5e7261860a015
itshdpog <mateszkielmas@gmail.com>
2022-03-06 16:13:21 +0100

    new sprite sheet

--- assets/preload/data/MVP/script.lua
+++ assets/preload/data/MVP/script.lua
@@ -1,30 +1,18 @@
-
-
-function onEndSong()
-	if not allowEnd and isStoryMode then
-		startVideo('last');
-		allowEnd = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
-
 local allowCountdown = false
 function onStartCountdown()
 	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('third');
+		startVideo('second');
 		allowCountdown = true;
 		return Function_Stop;
 	end
 	return Function_Continue;
 end
 
-
 --healthdrain code
 function opponentNoteHit()
     health = getProperty('health')
     if getProperty('health') > 0.05 then
-        setProperty('health', health- 0.03);
+        setProperty('health', health- 0.0135);
     end
 end
 --end of healthdrain code
@@ -97,4 +85,3 @@ end
 			doTweenX('MoveOutFive', 'JukeBoxSubText2', -450, 1.5, 'CircInOut')
 		end
 	end
-
